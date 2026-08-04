94f052b91b3490356c854b88a7e67f9a1836a412
Kennedy Pegorara <pegorara@proton.me>
2026-08-04 18:42:47 -0300

    chore: configuração inicial do projeto Spring Boot

---
+++ src/main/java/com/kennedy/tarifa_agua_api/TarifaAguaApiApplication.java
@@ -0,0 +1,13 @@
+package com.kennedy.tarifa_agua_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarifaAguaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarifaAguaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kennedy/tarifa_agua_api/TarifaAguaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennedy.tarifa_agua_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarifaAguaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
